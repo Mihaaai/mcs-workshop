2bb3bfbb02c4746a90beac3709c1589a7e0d6aff
Bogdan Alexandrescu <bogdanalexandrescu96@gmail.com>
2018-10-13 12:18:47 +0300

    draw rect on canvas

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 window.onload = function() {
     input = document.getElementById('image_input');
     input.addEventListener('change', load_image);
+
+    button = document.getElementById('draw');
+    button.addEventListener('click', function(e) {
+        drawRect([20, 20, 200, 200], true);
+    });
 };
 
 function load_image(e) {
     var target = e.target;
     var files = target.files;
     var canvas = document.getElementById('image_canvas');
-    var context = canvas.getContext('2d');
+    var ctx = canvas.getContext('2d');
 
     // FileReader support
     if (files && files.length) {
@@ -16,9 +21,23 @@ function load_image(e) {
             image = new Image();
             image.src = fileReader.result;
             image.onload = function(e) {
-                context.drawImage(image, 0, 0);
+                ctx.drawImage(image, 0, 0);
             };
         };
         fileReader.readAsDataURL(files[0]);
     }
+}
+
+function drawRect(coords, fill = true) {
+    canvas = document.getElementById('image_canvas');
+    ctx = canvas.getContext('2d');
+    //ctx.rect(Object.values(coords));
+    ctx.rect(...coords);
+    if (fill) {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(...coords);
+    }
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 3;
+    ctx.stroke();
 }
