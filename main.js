b38499bcd8a625bf82007e84deee06dac18f2b77
Bogdan Alexandrescu <bogdanalexandrescu96@gmail.com>
2018-10-13 13:24:28 +0300

    add ajax

--- main.js
+++ main.js
@@ -18,14 +18,17 @@ function load_image(e) {
     if (files && files.length) {
         var fileReader = new FileReader();
         fileReader.onload = function() {
+            makeDetectRequest(fileReader.result);
             image = new Image();
             image.src = fileReader.result;
+
             image.onload = function(e) {
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
                 ctx.drawImage(image, 0, 0);
             };
         };
         fileReader.readAsDataURL(files[0]);
+        //fileReader.readAsBinaryString(files[0]);
     }
 }
 
@@ -45,4 +48,22 @@ function drawRect(coords, name = 'Mihai', fill = true) {
         ctx.fillStyle = 'red';
         ctx.fillText(name, coords[0] + coords[2] / 3, coords[1] - 5);
     }
+}
+
+function makeDetectRequest(imageData) {
+    $.ajax({
+        type: 'POST',
+        url: 'https://northeurope.api.cognitive.microsoft.com/face/v1.0/detect',
+        contentType: 'application/octet-stream',
+        headers: {
+            'Ocp-Apim-Subscription-Key': '512f1b0661904e25918896ee76b0f417'
+        },
+        data: '{"url": ' + '"' + imageData + '"}',
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            alert(error);
+        }
+    });
 }
